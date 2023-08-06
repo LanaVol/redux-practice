294f35e5478c5f02849198c066f763fa4dd0336c
LanaVol <svolosh4yk@gmail.com>
2023-08-07 00:18:39 +0300

    add to redux to component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
   return (
     <div className="App">
-      <h1>Hello1</h1>
+      <div style={{ fontSize: "2rem" }}>{cash}</div>
+      <div style={{ display: "flex", gap: "0.5rem" }}>
+        <button onClick={() => addCash(Number(prompt()))}>
+          Поповнити рахунок
+        </button>
+        <button onClick={() => getCash(Number(prompt()))}>Зняти кошти</button>
+      </div>
     </div>
   );
 }
