97251f425acb251f1c90194a2612b8daede10ee5
LanaVol <svolosh4yk@gmail.com>
2023-08-08 18:52:51 +0300

    reducer add/remove customers & refactoring code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import {
+  addCustomerAction,
+  removeCustomerAction,
+} from "./store/customerReducer";
 
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector((state) => state.cash.cash);
+  const customers = useSelector((state) => state.customers.customers);
 
   const addCash = (cash) => {
     dispatch({ type: "ADD_CASH", payload: cash });
@@ -12,15 +17,59 @@ function App() {
     dispatch({ type: "GET_CASH", payload: cash });
   };
 
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    };
+    dispatch(addCustomerAction(customer));
+  };
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id));
+  };
+
   return (
     <div className="App">
       <div style={{ fontSize: "2rem" }}>{cash}</div>
-      <div style={{ display: "flex", gap: "0.5rem" }}>
+      <div
+        style={{
+          display: "flex",
+          gap: "0.5rem",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <button onClick={() => addCash(Number(prompt()))}>
           Поповнити рахунок
         </button>
         <button onClick={() => getCash(Number(prompt()))}>Зняти кошти</button>
+        <button onClick={() => addCustomer(prompt())}>Додати клієнта</button>
+        <button onClick={() => {}}>Видалити клієнта</button>
       </div>
+      {customers.length > 0 ? (
+        <div
+          style={{
+            paddingTop: "10px",
+          }}
+        >
+          {customers.map((customer) => (
+            <div
+              key={customer.id}
+              style={{
+                fontSize: "2rem",
+                border: "1px solid black",
+                padding: "10px",
+              }}
+              onClick={() => removeCustomer(customer)}
+            >
+              {customer.name}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div style={{ fontSize: "2rem" }}>Клієнти відсутні</div>
+      )}
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from "redux";
 import { cashReducer } from "./cashReducer";
 import { customerReducer } from "./customerReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 //кореневий редюсер
 const rootReducer = combineReducers({
@@ -8,4 +9,6 @@ const rootReducer = combineReducers({
   customers: customerReducer,
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
+
+// другим параметром передано підключення інструментів розробника в браузері, встановивши додатковий пакет для браузеру можна відслудковувати стан компонентів в devtools browser redux
